5ac090595ad1fda01850feeeb99836b18e8e113d
kentChris <kentchris6@gmail.com>
2024-03-06 11:26:13 +0700

    fix few stuf

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,18 @@ import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
 import { ProdukModule } from './produk/produk.module';
+import { config } from 'dotenv';
 
+config();
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
       port: 3306,
-      username: process.env.SQL_USERNAME,
-      password: process.env.SQL_PASSWORD,
-      database: process.env.SQL_DATABASE,
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
       entities: ["dist/**/*.entity{.ts,.js}"],
       synchronize: true,
     }),
